b370091b56613e47bcf8c75631d174a32560c360
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-01-10 14:42:02 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
   from fastapi import FastAPI
-  from main import main # Import your FastAPI application
+  from main import app # Import your FastAPI application
 
   if __name__ == "__main__":
       import uvicorn
-      uvicorn.run(main, host="0.0.0.0", port=8000)
+      uvicorn.run(app, host="0.0.0.0", port=8000)
