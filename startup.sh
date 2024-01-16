b631395d3607d39a77ab6047c909346fab459461
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-01-16 11:23:44 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,1 @@
-  from fastapi import FastAPI
-  from main import app # Import your FastAPI application
-
-  if __name__ == "__main__":
-      import uvicorn
-      uvicorn.run(app, host="0.0.0.0", port=8000)
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
